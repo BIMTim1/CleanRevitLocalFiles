355261c4ddf6f94f34018c0226ef43093b0e1d98
BIMTim1 <119504422+BIMTim1@users.noreply.github.com>
2023-02-22 16:54:54 -0600

    Added Remove-RevitLocals function
    Created the Remove-RevitLocals function to simplify the script and reduce the amount of repeated code. Full code notes have also been added to better clarify process of script.

--- CleanRevitTempFiles.ps1
+++ CleanRevitTempFiles.ps1
@@ -5,6 +5,24 @@ $pathMain = "C:\Users\$user_file\AppData\Local\Autodesk\Revit"
 $pathTemp = "C:\Users\$user_file\AppData\Local\Temp"
 $revFolders = Get-ChildItem -Path "$pathMain"
 
+#create function to delete local Revit content
+function Remove-RevitLocals($RevitFolders, $MainPath, $TempFilePath, $UserName) {
+	#iterate through list of local folders
+	foreach ($folder in $RevitFolders) {
+		#get folder name
+		$fName = $folder.Name
+		if ($fName.Contains("Revit")) {
+			#delete all files in the CollaborationCache folder
+			Get-ChildItem -Path "$MainPath\$fName\CollaborationCache" -Recurse | ForEach-Object {Remove-Item -Recurse -Path $_.FullName}
+			#delete all files in the Journals folder
+			Get-ChildItem -Path "$MainPath\$fName\Journals" -Recurse | ForEach-Object {Remove-Item -Recurse -Path $_.FullName}
+		}
+	}
+
+	Get-ChildItem -Path $TempFilePath -Recurse | ForEach-Object {Remove-Item -Recurse -Path $_.FullName -ErrorAction SilentlyContinue}
+
+	Get-ChildItem -Path "C:\`$Recycle.Bin" -Force | Remove-Item -Recurse -ErrorAction SilentlyContinue
+}
 #get Revit process in background
 $rev = "Revit"
 $rev_process = Get-Process -Name $rev -ErrorAction SilentlyContinue
@@ -12,43 +30,25 @@ $rev_process = Get-Process -Name $rev -ErrorAction SilentlyContinue
 if ($rev_process) {
 	$userInput = Read-Host "A Revit process is still running.`nType Y to close Revit. Type N to cancel tool."
 	if ("Y" -eq $userInput) {
+		#close all Revit processes if accepted by user
 		$rev_process.CloseMainWindow()
-		#pause while Revit closes
-		Start-Sleep -Seconds 10
 		
-		foreach ($folder in $revFolders)
-		{
-			$fName = $folder.Name
-			if($fName.Contains("Revit"))
-			{
-				Get-ChildItem -Path "$pathMain\$fName\CollaborationCache" -Recurse | ForEach-Object {Remove-Item -Recurse -Path $_.FullName}
-				Get-ChildItem -Path "$pathMain\$fName\Journals" -Recurse | ForEach-Object {Remove-Item -Recurse -Path $_.FullName}
-			}
-		}
+		#pause for 10 seconds while Revit closes
+		Start-Sleep -Seconds 10
 
-		Get-ChildItem -Path $pathTemp -Recurse | ForEach-Object {Remove-Item -Recurse -Path $_.FullName -ErrorAction SilentlyContinue}
-
-		Get-ChildItem -Path "C:\`$Recycle.Bin" -Force | Remove-Item -Recurse -ErrorAction SilentlyContinue
-
-		Write-Host "All local Revit content has been removed from $user_file."
-
+		#create function to delete local Revit content after Revit has closed
+		Remove-RevitLocals -RevitFolders $revFolders -MainPath $pathMain -TempFilePath $pathTemp -UserName $user_file
+		#notify user function is complete
+		Write-Host "All local Revit content has been removed from $UserName."
+		#prompt user to close PowerShell
 		Read-Host -Prompt "Press Enter to continue"
 	}
 }
 else {
-	foreach ($folder in $revFolders) {
-		$fName = $folder.Name
-		if($fName.Contains("Revit")) {
-			Get-ChildItem -Path "$pathMain\$fName\CollaborationCache" -Recurse | ForEach-Object {Remove-Item -Recurse -Path $_.FullName}
-			Get-ChildItem -Path "$pathMain\$fName\Journals" -Recurse | ForEach-Object {Remove-Item -Recurse -Path $_.FullName}
-		}
-	}
-
-	Get-ChildItem -Path $pathTemp -Recurse | ForEach-Object {Remove-Item -Recurse -Path $_.FullName -ErrorAction SilentlyContinue}
-
-	Get-ChildItem -Path "C:\`$Recycle.Bin" -Force | Remove-Item -Recurse -ErrorAction SilentlyContinue
-
-	Write-Host "All local Revit content has been removed from $user_file."
-
+	#create function to delete local Revit content
+	Remove-RevitLocals -RevitFolders $revFolders -MainPath $pathMain -TempFilePath $pathTemp -UserName $user_file
+	#notify user function is complete
+	Write-Host "All local Revit content has been removed from $UserName."
+	#prompt user to close PowerShell
 	Read-Host -Prompt "Press Enter to continue"
 }
